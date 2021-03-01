7ec17fcb92901731bd367f5ecb88463103be64e3
seff0 <76668281+seff0@users.noreply.github.com>
2021-02-28 19:55:13 -0700

    typo in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ("Use It or Bleus It Burger");
-INSERT INTO burgers (name) VALUES ("She's a Super Leek Burger");
-INSERT INTO burgers (name) VALUES ("It Takes Bun to Know Bun Burger");
+INSERT INTO burgers (burger_name) VALUES ("Use It or Bleus It Burger");
+INSERT INTO burgers (burger_name) VALUES ("She's a Super Leek Burger");
+INSERT INTO burgers (burger_name) VALUES ("It Takes Bun to Know Bun Burger");
