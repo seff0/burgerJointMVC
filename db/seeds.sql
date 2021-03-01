fb8628b74ec36f4c8bb1b6c16d4f7b3a010ce9fb
seff0 <76668281+seff0@users.noreply.github.com>
2021-02-28 19:53:42 -0700

    create schema and seeds files in db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("Use It or Bleus It Burger");
+INSERT INTO burgers (name) VALUES ("She's a Super Leek Burger");
+INSERT INTO burgers (name) VALUES ("It Takes Bun to Know Bun Burger");
