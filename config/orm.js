e1e384621014f706b64a2e4d5b08ea960c9bdc5c
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-01 13:50:21 -0700

    construct orm function

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,51 @@
 const connection = require("./connection.js");
 
+const printQuestionMarks = (num) => {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+};
+
 const orm = {
-  selectAll() {},
-  insertOne() {},
-  updateOne() {},
+  selectAll(table, cb) {
+    let query = `SELECT * FROM ${table}`;
+    connection.query(query, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+  insertOne(table, cols, vals, cb) {
+    let query = `INSERT INTO ${table}`;
+    query += " (";
+    query += cols;
+    query += ") ";
+    query += "VALUES (";
+    query += printQuestionMarks(vals.length);
+    query += ")";
+
+    connection.query(query, vals, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+  updateOne(table, cols, condition, cb) {
+    let query = `UPDATE ${table}`;
+    query += " SET ";
+    query += cols;
+    query += " WHERE ";
+    query += condition;
+
+    connection.query(query, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
 };
 
 module.exports = orm;
