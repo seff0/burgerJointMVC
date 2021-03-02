446363400e3b43c06510938bee2798f60e785bd6
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-01 22:17:12 -0700

    testing functionality, fixed broken img and resize

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -11,14 +11,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/burgers", (req, res) => {
-  burger.insertOne("name", [req.body.name], (result) => {
+  burger.insertOne("burger_name", [req.body.name], (result) => {
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", (req, res) => {
   const condition = `id = ${req.params.id}`;
-  burger.updateOne("burgers", "devoured", condition, (result) => {
+  burger.updateOne("devoured = false", condition, (result) => {
     if (result.changedRows == 0) {
       return res.status(404).end();
     }
